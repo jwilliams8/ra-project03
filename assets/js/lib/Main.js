166bf78a3b1d6cc15aa5d6d92b048d7561e820e0
Jesse Williams <jwilliams8@me.com>
2017-05-09 10:10:20 -0400

    Deleting assets

--- assets/js/lib/Main.js
+++
@@ -1,7 +0,0 @@
-export default class Main {
-  constructor() {
-    console.log("I'm importing from Main!");
-
-     
-    }
-  }
--- assets/js/main/index.js
+++
@@ -1,3 +0,0 @@
-import App from "../src/app";
-
-let app = new App();
